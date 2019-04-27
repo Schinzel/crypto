de8e7da5c7e1b8fa240726f13b560a9c93c4702d
Henrik Schinzel <henrik@schinzel.io>
2019-04-27 07:04:40 +0200

    Handled warning

--- src/main/java/io/schinzel/crypto/cipher/Aes256GcmException.java
+++ src/main/java/io/schinzel/crypto/cipher/Aes256GcmException.java
@@ -4,6 +4,7 @@ package io.schinzel.crypto.cipher;
  * The purpose of this class is to throw a custom exception instead of a RuntimeException.
  * This as to avoid sonar lint warnings about not using generic exception.
  */
+@SuppressWarnings("serial")
 class Aes256GcmException extends RuntimeException {
     Aes256GcmException(String message) {
         super(message);
