1172998a930c175ea60a1158379790ffb4f9a0f9
Henrik Schinzel <henrik@schinzel.io>
2019-04-27 13:25:48 +0200

    Test added

---
+++ src/test/java/io/schinzel/crypto/encoding/base62/BitOutputStreamTest.java
@@ -0,0 +1,22 @@
+package io.schinzel.crypto.encoding.base62;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class BitOutputStreamTest {
+
+    @Test
+    public void getBitsCountUpToByte_Offset8_0() {
+        int output = BitOutputStream.getBitsCountUpToByte(8);
+        assertThat(output).isEqualTo(0);
+    }
+
+    @Test
+    public void getBitsCountUpToByte_Offset12_4() {
+        int output = BitOutputStream.getBitsCountUpToByte(12);
+        assertThat(output).isEqualTo(4);
+    }
+
+}
